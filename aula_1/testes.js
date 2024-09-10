aca962130bad221ad004348f76c928276ea9ed21
seu nome aqui <carloscelestino93@gmail.com>
2024-09-10 12:20:47 -0300

    primeiro commit
    config basicas

---
+++ aula_1/testes.js
@@ -0,0 +1,19 @@
+// Abra o Prompt de Comando ou PowerShell e execute o seguinte comando para criar uma nova aplicação React:
+// npx create-react-app hello-world-react
+
+
+
+// Após o processo de criação, entre no diretório do projeto:
+// cd hello-world-react
+
+
+// Inicie o servidor de desenvolvimento React:
+// npm start
+
+
+
+// Abra o Prompt de Comando ou Terminal no diretório onde você quer criar o projeto.
+// Execute o seguinte comando para criar o projeto React com Vite:
+//          npm create vite@latest
+
+
